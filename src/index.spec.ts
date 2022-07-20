3a7b4940c00b5ea374d640da345cda703d0d9dc4
Moaad Fattali <moaad.fattali@ovrsea.com>
2022-07-20 08:50:35 +0100

    step 3

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,16 +1,19 @@
 // @ts-ignore see https://github.com/jest-community/jest-extended#setup
 import * as matchers from "jest-extended";
-import { CoffeeMachineInput, enhanceCoffeeOrder, UserInput } from ".";
+import { EnhancedDrinkOrder, enhanceCoffeeOrder, UserInput } from ".";
 
 expect.extend(matchers);
 describe("Coffee machine user interface", () => {
   it("Should be able to correctly give instruction for a drink without sugar (and therefore without stick)", () => {
     const userInput: UserInput = {
-      drink: "tea",
-      sugarQuantity: 0,
+      input: {
+        drink: "tea",
+        sugarQuantity: 0,
+      },
+      money: { cents: 40 },
     };
 
-    const expectedMachineInput: CoffeeMachineInput = {
+    const expectedMachineInput: EnhancedDrinkOrder = {
       drink: "tea",
       stickOption: "no stick",
       sugarQuantity: 0,
@@ -21,14 +24,83 @@ describe("Coffee machine user interface", () => {
 
   it("Should be able to correctly give instruction for a drink with one sugar (and therefore with a stick)", () => {
     const userInput: UserInput = {
-      drink: "coffee",
-      sugarQuantity: 1,
+      input: {
+        drink: "coffee",
+        sugarQuantity: 1,
+      },
+      money: { cents: 60 },
     };
 
-    const expectedMachineInput: CoffeeMachineInput = {
+    const expectedMachineInput: EnhancedDrinkOrder = {
       drink: "coffee",
       stickOption: "with stick",
       sugarQuantity: 1,
+    };
+
+    expect(enhanceCoffeeOrder(userInput)).toEqual(expectedMachineInput);
+  });
+
+  it("Should give instruction to display error message to the machine if no money is given (case no balance)", () => {
+    const userInput: UserInput = {
+      input: {
+        drink: "coffee",
+        sugarQuantity: 0,
+      },
+      money: { cents: 0 },
+    };
+
+    expect(enhanceCoffeeOrder(userInput)).toEqual({ errorMessage: "Insufficient balance: 0.6 € missing" });
+  });
+
+  it("Should give instruction to display error message to the machine if no money is given (case insufficient balance)", () => {
+    const userInput: UserInput = {
+      input: {
+        drink: "coffee",
+        sugarQuantity: 0,
+      },
+      money: { cents: 40 },
+    };
+
+    expect(enhanceCoffeeOrder(userInput)).toEqual({ errorMessage: "Insufficient balance: 0.2 € missing" });
+  });
+
+  it("Should give instruction to display error message to the machine if no money is given (case insufficient balance)", () => {
+    const userInput: UserInput = {
+      input: {
+        drink: "tea",
+        sugarQuantity: 1,
+      },
+      money: { cents: 30 },
+    };
+
+    expect(enhanceCoffeeOrder(userInput)).toEqual({ errorMessage: "Insufficient balance: 0.1 € missing" });
+  });
+
+  it("Should give instruction to display error message to the machine if no money is given (case insufficient balance)", () => {
+    const userInput: UserInput = {
+      input: {
+        drink: "chocolate",
+        sugarQuantity: 1,
+      },
+      money: { cents: 30 },
+    };
+
+    expect(enhanceCoffeeOrder(userInput)).toEqual({ errorMessage: "Insufficient balance: 0.2 € missing" });
+  });
+
+  it("Should give correctly give instructions if too much money is given", () => {
+    const userInput: UserInput = {
+      input: {
+        drink: "tea",
+        sugarQuantity: 0,
+      },
+      money: { cents: 800 },
+    };
+
+    const expectedMachineInput: EnhancedDrinkOrder = {
+      drink: "tea",
+      stickOption: "no stick",
+      sugarQuantity: 0,
     };
 
     expect(enhanceCoffeeOrder(userInput)).toEqual(expectedMachineInput);
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,51 @@
-export type Drink = "tea" | "chocolate" | "coffee";
+export type ColdDrink = "orange juice";
+
+export type HotDrink = "tea" | "chocolate" | "coffee";
+
+export type Drink = ColdDrink | HotDrink;
+
+export type SugarableDrink = "tea" | "chocolate" | "coffee";
+
+export type NonSugarableDrink = "orange juice";
 
 export type NoSugar = 0;
+
 export type WithSugar = 1 | 2;
 
 export type SugarQuantity = 0 | 1 | 2;
 
 export type StickOption = "with stick" | "no stick";
 
-export type UserInput = { drink: Drink; sugarQuantity: SugarQuantity };
+export type UserOrder<DrinkType> = { drink: DrinkType; sugarQuantity: SugarQuantity };
 
-export type CoffeeMachineInput =
-  | { drink: Drink; sugarQuantity: NoSugar; stickOption: "no stick" }
-  | { drink: Drink; sugarQuantity: WithSugar; stickOption: "with stick" };
+export type UserInput<DrinkType> = { input: UserOrder<DrinkType>; money: { cents: number } };
 
-export const enhanceCoffeeOrder = ({ drink, sugarQuantity }: UserInput): CoffeeMachineInput => {
+export type DrinkOrderError = { errorMessage: string };
+
+export type NoSugarOption = { sugarQuantity: NoSugar; stickOption: "no stick" };
+
+export type SugarOptions<DrinkType extends Drink> = DrinkType extends SugarableDrink
+  ? { sugarQuantity: WithSugar; stickOption: "with stick" }
+  : { sugarQuantity: NoSugar; stickOption: "no stick" };
+
+export type EnhancedDrinkOrder<DrinkType extends Drink> = { drink: DrinkType } & SugarOptions<DrinkType>;
+
+const DRINKS_PRICES_CENTS: Record<Drink, number> = {
+  coffee: 60,
+  tea: 40,
+  chocolate: 50,
+};
+
+export const enhanceCoffeeOrder = <DrinkType extends Drink>({
+  input: { drink, sugarQuantity },
+  money,
+}: UserInput<DrinkType>): EnhancedDrinkOrder<DrinkType> | DrinkOrderError => {
+  const price = DRINKS_PRICES_CENTS[drink];
+
+  if (money.cents < price) {
+    return { errorMessage: `Insufficient balance: ${(price - money.cents) / 100} € missing` };
+  }
+
   if (sugarQuantity === 0) {
     return {
       drink,
